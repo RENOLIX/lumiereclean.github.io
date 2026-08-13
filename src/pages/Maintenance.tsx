6d996da112d610a7670ea63d7aad83956fab2559
asoual12 <alger12c200@gmail.com>
2026-08-13 13:13:58 +0100

    Temporarily disable site

---
+++ src/pages/Maintenance.tsx
@@ -0,0 +1,9 @@
+import "./maintenance.css";
+
+export default function Maintenance() {
+  return <main className="maintenance-page">
+    <div className="maintenance-content">
+      <p>Le site reviendra après le paiement.</p>
+    </div>
+  </main>;
+}
