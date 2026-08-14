d796911e390ce94d84dd19e388a31b2ebbc1c3ea
asoual12 <alger12c200@gmail.com>
2026-08-14 01:35:01 +0100

    Temporarily replace site with payment notice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Index from "./pages/Index";
-import AboutPage from "./pages/about/page";
-import ChantierPage from "./pages/chantier/page";
-import DevisPage from "./pages/devis/page";
-import ServicesPage from "./pages/services/page";
-import NotFound from "./pages/NotFound";
-import PrixPage from "./pages/PrixPage";
+import "./maintenance.css";
 
 export default function App() {
-  return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/nettoyage-apres-chantier" element={<ChantierPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/devis" element={<DevisPage />} />
-        <Route path="/prix" element={<PrixPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <main className="maintenance-page">Quand vous payerez le prestataire, le site reviendra.</main>;
 }
